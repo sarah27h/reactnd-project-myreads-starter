69534023711c7d9521e270ee3ac80bc5a16dc587
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-07-14 19:47:17 +0200

    Refactor: delete jsx as its default to be blank

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -24,26 +24,7 @@ class SearchBooks extends Component {
                 </div>
                 <div className="search-books-results">
                     <ol className="books-grid">
-                    {this.props.books.map((book) => (
-                        <li>
-                            <div className="book">
-                                <div className="book-top">
-                                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
-                                    <div className="book-shelf-changer">
-                                    <select>
-                                        <option value="move" disabled>Move to...</option>
-                                        <option value="currentlyReading">Currently Reading</option>
-                                        <option value="wantToRead">Want to Read</option>
-                                        <option value="read">Read</option>
-                                        <option value="none">None</option>
-                                    </select>
-                                    </div>
-                                </div>
-                                <div className="book-title">{book.title}</div>
-                                <div className="book-authors">{book.authors}</div>
-                            </div>
-                        </li>
-                        ))}
+                    
                     </ol>
                 </div>
             </div>
