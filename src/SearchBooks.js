e3bc32db9358d959a74edfca0ad3558b9cf131c2
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-07-16 21:49:38 +0200

    Feat: Search results aren't shown> text is deleted

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -54,6 +54,41 @@ class SearchBooks extends Component {
     
     
     render() {
+
+        if(this.state.query === '') {
+
+            return(
+                <div className="search-books">
+                    <div className="search-books-bar">
+                        <Link to="/" className="close-search">Close</Link>
+                        <div className="search-books-input-wrapper">
+                        {/*
+                            NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                            You can find these search terms here:
+                            https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+    
+                            However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                            you don't find a specific author or title. Every search is limited by search terms.
+                        */}
+    
+                        <input type="text" 
+                        placeholder="Search by title or author"
+                        value={this.state.query}
+                        onChange={(event) => {this.updateQuery(event.target.value)}} />
+                        
+                        </div>
+                    </div>
+                    <div className="search-books-results">
+                        
+                        <ol className="books-grid">
+                        </ol>
+                    </div>
+                </div>
+    
+            )}
+
+        else {
+
         return(
             <div className="search-books">
                 <div className="search-books-bar">
@@ -105,7 +140,7 @@ class SearchBooks extends Component {
                 </div>
             </div>
 
-        )
+        )}
     }
 }
 
