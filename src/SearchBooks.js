516ae87ece0b86e8c8e5e2c8214dd594f9641c78
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-07-17 03:22:47 +0200

    Handle: book doesn't have a thumbnail or an author

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -86,7 +86,15 @@ class SearchBooks extends Component {
                             <li key={book.id}>
                                 <div className="book">
                                     <div className="book-top">
-                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
+
+                                        {typeof book.imageLinks === 'undefined'? 
+                                        (<div className="book-cover" style={{ width: 128, height: 193, 
+                                            backgroundImage: `url(http://via.placeholder.com/128x193?text=no%20image)`}}></div>
+                                          )  : (
+                                             <div className="book-cover" style={{ width: 128, height: 193, 
+                                            backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
+                                        )}
+                                    
                                         <div className="book-shelf-changer">
     
                                         <select value='currentlyReading' onChange={(event) => {this.setState({value: event.target.value}); this.props.onShelfChange(book, book.shelf, event.target.value)} }>
