2a8b323916ed76fc8fb7410930c2f01a52687323
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-07-19 18:33:07 +0200

    Retactor: book doesn't have a thumbnail, an author

--- src/Book.js
+++ src/Book.js
@@ -1,32 +1,44 @@
 import React, { Component } from 'react';
 
 class Book extends Component {
-    
-    state = {value: ''};
   
     render() {
         return(
             
-                <div className="book">
-                    <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})`}}></div>
-                        <div className="book-shelf-changer">
+            <div className="book">
+                <div className="book-top">
 
-                        <select defaultValue={this.props.book.shelf} onChange={event => this.props.onShelfChange(this.props.book, this.props.book.shelf, event.target.value)}>>
-                            <option value="move" disabled>Move to...</option>
-                            <option value="currentlyReading" selected={this.props.book.shelf === 'currentlyReading'}>Currently Reading</option>
-                            <option value="wantToRead" selected={this.props.book.shelf === 'wantToRead'}>Want to Read</option>
-                            <option value="read" selected={this.props.book.shelf === 'read'}>Read</option>
-                            <option value="none">None</option>
-                        </select>
+                    {typeof this.props.book.imageLinks === 'undefined'? 
+                    (<div className="book-cover" style={{ width: 128, height: 193, 
+                        backgroundImage: `url(http://via.placeholder.com/128x193?text=no%20image)`}}></div>
+                        )  : (
+                            <div className="book-cover" style={{ width: 128, height: 193, 
+                        backgroundImage: `url(${this.props.book.imageLinks.thumbnail})`}}></div>
+                    )}
+                    {/* <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})`}}></div> */}
+                    <div className="book-shelf-changer">
 
-                        </div>
+                    <select onChange={event => this.props.onShelfChange(this.props.book, this.props.book.shelf, event.target.value)}>                                       
+                        <option value="move" disabled>Move to...</option>
+                        <option value="currentlyReading" selected={this.props.book.shelf === 'currentlyReading'}>Currently Reading</option>
+                        <option value="wantToRead" selected={this.props.book.shelf === 'wantToRead'}>Want to Read</option>
+                        <option value="read" selected={this.props.book.shelf === 'read'}>Read</option>
+                        <option value="none" selected={typeof this.props.book.shelf === 'undefined'}>None</option>
+                    </select>
+
                     </div>
-                    <div className="book-title">{this.props.book.title}</div>
-                    <div className="book-authors">{this.props.book.authors}</div>
                 </div>
-            
-           
+                <div className="book-title">{this.props.book.title}</div>
+                
+                {typeof this.props.book.authors === 'undefined'?
+                (
+                    <div className="book-authors">{'Name Unkown'}</div>
+                ) : (
+                    <div className="book-authors">{this.props.book.authors}</div>
+                )}
+
+            </div>
+        
         )
     }
 
